62c7468eb179d12ff5ce622c5bb7c057bf8bd85f
Jalokan <150829310+Jalokan@users.noreply.github.com>
2024-03-24 13:24:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,59 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-  return 0;
+int countPairs1(int* arr, int len, int value) {
+    int count = 0;
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if ((*(arr + i) + *(arr + j)) == value) {
+                count++;
+            }
+        }
+    }
+    return count;
 }
-int countPairs2(int *arr, int len, int value) {
-  return 0;
+int countPairs2(int* arr, int len, int value) {
+    int count = 0;
+    int rightborder = len - 1;
+    for (int i = 0; i < (rightborder); i++) {
+        for (int j = (rightborder); j > i; j--) {
+            if ((*(arr + i) + *(arr + j)) > value) {
+                rightborder--;
+                continue;
+            }
+            else if ((*(arr + i) + *(arr + j)) == value) {
+                count++;
+            }
+        }
+    }
+    return count;
 }
-int countPairs3(int *arr, int len, int value) {
-  return 0;
+int countPairs3(int* arr, int len, int value) {
+    int count = 0;
+    int leftborder = 0;
+    int rightborder = len - 1;
+    for (int c = 0; c < len - 1; c++) {
+        while (leftborder <= rightborder) {
+            int center = (leftborder + rightborder) / 2;
+            if ((*(arr + c) + *(arr + center)) == value) {
+                count++;
+                int i = center - 1;
+                while ((*(arr + c) + *(arr + (i))) == value) {
+                    count++;
+                    i--;
+                }
+                int j = center + 1;
+                while ((*(arr + c) + *(arr + (j))) == value) {
+                    count++;
+                    j++;
+                }
+            }
+            else if (*(arr + center) < value) {
+                leftborder = center + 1;
+            }
+            else {
+                rightborder = center - 1;
+            }
+        }
+        return count;
+    }
+
 }
